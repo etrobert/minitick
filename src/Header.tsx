cc344a4ede352b6c1f6ed759bc64bfca0da5e257
erobert <etiennerobert33@gmail.com>
2022-05-27 23:19:05 +0200

    Show intent with emojis

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,10 +1,21 @@
 import { useRecoilValue } from "recoil";
 import { playerIntentState } from "./atoms";
 
+const emojis = {
+  up: "⬆️",
+  down: "⬇️",
+  right: "➡️",
+  left: "⬅️",
+};
+
 const Header = () => {
   const playerIntent = useRecoilValue(playerIntentState);
 
-  return <div className="Header">{`PlayerIntent: ${playerIntent}`}</div>;
+  return (
+    <div className="Header">{`Player: ${
+      playerIntent === null ? "❌" : emojis[playerIntent]
+    }`}</div>
+  );
 };
 
 export default Header;
