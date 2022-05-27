c03bf6a4d01c6714ae0b55d123c8b55855425c2a
erobert <etiennerobert33@gmail.com>
2022-05-27 21:57:46 +0200

    Add keyboard events to move the player

--- src/usePlayerPosition.ts
+++ src/usePlayerPosition.ts
@@ -1,7 +1,12 @@
 import { useState } from "react";
+import { useHotkeys } from "react-hotkeys-hook";
 
 const usePlayerPosition = () => {
-  const [playerPosition, setPlayerPosition] = useState({ x: 0, y: 0 });
+  const [playerPosition, setPlayerPosition] = useState({ x: 0, y: 3 });
+  useHotkeys("up", () => setPlayerPosition(({ x, y }) => ({ x, y: y - 1 })));
+  useHotkeys("down", () => setPlayerPosition(({ x, y }) => ({ x, y: y + 1 })));
+  useHotkeys("right", () => setPlayerPosition(({ x, y }) => ({ x: x + 1, y })));
+  useHotkeys("left", () => setPlayerPosition(({ x, y }) => ({ x: x - 1, y })));
 
   return playerPosition;
 };
