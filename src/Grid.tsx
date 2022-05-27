2313b9357e46c75722f4e2c9f08a3a726ef6fc12
erobert <etiennerobert33@gmail.com>
2022-05-27 21:17:00 +0200

    Add cells

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -1,3 +1,14 @@
-const Grid = () => <div className="Grid">Grid</div>;
+import range from "lodash/range";
+
+const size = 4;
+const cells = range(size * size);
+
+const Grid = () => (
+  <div className="Grid">
+    {cells.map((_, i) => (
+      <div key={i} style={{ width: 30, height: 30, backgroundColor: "pink" }} />
+    ))}
+  </div>
+);
 
 export default Grid;
