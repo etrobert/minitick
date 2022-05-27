e46262bed9baec3d63fe2437d5d3caaa3d7be99f
erobert <etiennerobert33@gmail.com>
2022-05-27 22:07:02 +0200

    Extract movePlayer

--- src/usePlayerPosition.ts
+++ src/usePlayerPosition.ts
@@ -1,12 +1,32 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { useHotkeys } from "react-hotkeys-hook";
+
+type Direction = "up" | "down" | "right" | "left";
 
 const usePlayerPosition = () => {
   const [playerPosition, setPlayerPosition] = useState({ x: 0, y: 3 });
-  useHotkeys("up", () => setPlayerPosition(({ x, y }) => ({ x, y: y - 1 })));
-  useHotkeys("down", () => setPlayerPosition(({ x, y }) => ({ x, y: y + 1 })));
-  useHotkeys("right", () => setPlayerPosition(({ x, y }) => ({ x: x + 1, y })));
-  useHotkeys("left", () => setPlayerPosition(({ x, y }) => ({ x: x - 1, y })));
+
+  const movePlayer = useCallback((direction: Direction) => {
+    switch (direction) {
+      case "up":
+        setPlayerPosition(({ x, y }) => ({ x, y: y - 1 }));
+        break;
+      case "down":
+        setPlayerPosition(({ x, y }) => ({ x, y: y + 1 }));
+        break;
+      case "right":
+        setPlayerPosition(({ x, y }) => ({ x: x + 1, y }));
+        break;
+      case "left":
+        setPlayerPosition(({ x, y }) => ({ x: x - 1, y }));
+        break;
+    }
+  }, []);
+
+  useHotkeys("up", () => movePlayer("up"));
+  useHotkeys("down", () => movePlayer("down"));
+  useHotkeys("right", () => movePlayer("right"));
+  useHotkeys("left", () => movePlayer("left"));
 
   return playerPosition;
 };
