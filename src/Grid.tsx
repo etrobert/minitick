fab090cb1290d928e80b702c48cad75a1deb60df
erobert <etiennerobert33@gmail.com>
2022-06-11 15:12:31 +0200

    Export equalPositions in helpers

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -4,18 +4,13 @@ import { useRecoilValue } from "recoil";
 import { gridSize } from "./constants";
 import { hitPositionState, playersPositionsState } from "./atoms";
 import Cell from "./Cell";
-import { adjacent } from "./helpers";
-
-import type { Position } from "./types";
+import { adjacent, equalPositions } from "./helpers";
 
 import "./Grid.css";
 
 const cells = range(gridSize)
   .map((y) => range(gridSize).map((x) => ({ x, y })))
   .flat();
-
-const equalPositions = (p1: Position, p2: Position) =>
-  p1.x === p2.x && p1.y === p2.y;
 
 const Grid = () => {
   const playersPositions = useRecoilValue(playersPositionsState);
--- src/helpers.ts
+++ src/helpers.ts
@@ -6,4 +6,7 @@ const adjacent = (pos1: Position, pos2: Position) =>
   (pos1.x === pos2.x - 1 && pos1.y === pos2.y) ||
   (pos1.x === pos2.x + 1 && pos1.y === pos2.y);
 
-export { adjacent };
+const equalPositions = (p1: Position, p2: Position) =>
+  p1.x === p2.x && p1.y === p2.y;
+
+export { adjacent, equalPositions };
